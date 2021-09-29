f382ece8b4d51040408c08e88a8a7f515374e430
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-29 10:22:26 -0300

    Update ComponentePai.vue

--- ComponentePai.vue
+++ ComponentePai.vue
@@ -1,68 +1,47 @@
 <template>
-    <money3 v-model="valueInput" @blur="atualizaBlur" v-bind="config"></money3>
-   
-</template>
-
-<script>
-import { Money3Component } from "v-money3";
-import { watch, ref } from "vue";
-
-export default {
-    components: { money3: Money3Component },
-    props: {
-        ViewModel: {
-            type: [String, Number],
-            required: false,
-            default: 0,
-        },
-        atualizaTab: {
-            type: Boolean,
-            required: false,
-            default: false,
-        },
-        config: {
-            type: Object,
-            required: false,
-            default() {
-                return {
-                    masked: false,
-                    prefix: "",//$
-                    suffix: "",//#
-                    thousands: ",",
-                    decimal: ".",
-                    precision: 2,
-                    disableNegative: false,
-                    disabled: false,
-                    min: null,
-                    max: null,
-                    allowBlank: false,
-                    minimumNumberOfCharacters: 0,
-                };
-            },
-        },
-    },
-    emits: ["reactRealTime"],
-    setup(props, context) {
-        let valueInput = ref(0);
-
-        const atualizaBlur = () => {
-            if (props.atualizaTab) {
-                context.emit("reactRealTime", valueInput.value);
-            }
-        };
-        watch(props, () => {
-            valueInput.value = props.ViewModel;
-        });
-        if (!props.atualizaTab) {
-            watch(valueInput, () => {
-                context.emit("reactRealTime", valueInput.value);
-            });
-        }
-        return {
-            valueInput,
-            atualizaBlur,
+    <div>
+        <format-imput-money
+            :ViewModel="ViewModel"
+            :atualizaTab="true"
+            :config="config"
+            @reactRealTime="reactRealTime"
+        ></format-imput-money>
+        {{ ViewModel }}
         
+    </div>
+</template>
+ 
+<script>
+import AutoNumericInput from "./formatImputMoney.vue";
+import { ref } from "vue";
+export default {
+    components: {
+        formatImputMoney
+    },
+    setup() {
+        let ViewModel = ref(0);
+        let config = ref({
+            masked: false,
+            prefix: "$",
+            suffix: "",
+            thousands: ",",
+            decimal: ".",
+            precision: 2,
+            disableNegative: false,
+            disabled: false,
+            min: null,
+            max: null,
+            allowBlank: false,
+            minimumNumberOfCharacters: 0,
+        });
+        const reactRealTime = (e) => {
+            ViewModel.value = e;
+        };
+        return {
+            ViewModel,
+            reactRealTime,
+            config,
         };
     },
 };
-</script>
+</script> 
